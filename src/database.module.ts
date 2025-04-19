0ae90b7a0b16b583587746e5ad49c9024ac86124
nghiemnx <nghiemnx.work@gmail.com>
2025-04-19 17:14:49 +0700

    feat: config env

--- src/database.module.ts
+++ src/database.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql', // Ensure the type is explicitly set
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Root@123',
-      database: 'OnlineShop',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '3306', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false, // Disable in production
     }),
